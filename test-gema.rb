dddd377b3351cd1afbcba86c1bebabf77227e453
LeandroT! <leandrogutierrezrc@gmail.com>
2020-04-09 09:58:38 -0300

    Create test-gema.rb
    Prueba de una gema llamada faker

---
+++ test-gema.rb
@@ -0,0 +1,11 @@
+#gem install bundler
+#bundle --version
+#bundle init
+#bundle install
+
+require 'faker'
+
+Faker::Movies::HarryPotter.methods?
+Faker::Movies::HarryPotter.spell
+
+#bundle exec irb
